418d6cd3642dbad6d6c6dccf4a83436a486b4242
TB831 <druu831@gmail.com>
2018-10-12 12:40:08 -0700

    rendering tags, two methods complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
+  state = {
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            { this.state.tags.map(tag => <li></li>)}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;

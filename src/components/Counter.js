f242bbc863cb0527d50ee6f41945bfebb3bf9bac
TB831 <druu831@gmail.com>
2018-10-12 14:49:29 -0700

    encapsulation on Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,25 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.counter.value
   };
 
-
-//   reunderTags() {
-//     if (this.state.tags.length === 0) return <p>There are not tags!</p>;
-
-//     return (
-//       <ul>
-//         {this.state.tags.map(tag => (
-//           <li key={tag}>{tag}</li>
-//         ))}
-//       </ul>
-//     );
-//   }
-
-  handleIncrement = (product) => {
-    this.setState({count: this.state.count + 1});
+  handleIncrement = product => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -33,24 +19,25 @@ class Counter extends Component {
         >
           Increment
         </button>
-        {/* <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul> */}
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 

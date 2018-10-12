7f4529d87ed0f7d86f8334d4fe53eb0085c2f961
TB831 <druu831@gmail.com>
2018-10-12 13:07:56 -0700

    handle increment button complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,40 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+
+  reunderTags() {
+    if (this.state.tags.lenght === 0) return <p>There are not tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({count: this.state.count + 1});
   };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         <ul>
-            { this.state.tags.map(tag => <li></li>)}
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
         </ul>
       </div>
     );
